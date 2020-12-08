fc5b27caa950437b84e4d5a075197ef250e9073b
CaoHienIT99 <64887341+CaoHienIT99@users.noreply.github.com>
2020-12-08 19:02:55 +0700

    add Homepage + Product

--- MobieStoreWeb/Controllers/HomeController.cs
+++ MobieStoreWeb/Controllers/HomeController.cs
@@ -42,6 +42,27 @@ namespace MobieStoreWeb.Controllers
             return View(_context.Products.OrderByDescending(p => p.PublishDate).Take(12));
         }
   
-     
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
---
+++ MobieStoreWeb/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MobieStoreWeb.Data;
+using MobieStoreWeb.Helpers;
+using MobieStoreWeb.Models;
+
+namespace MobieStoreWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string search, List<int> categories, List<int> manufacturers, int? page = 1)
+        {
+            ViewBag.Categories = await _context.Categories.ToListAsync();
+            ViewBag.Manufacturers = await _context.Manufacturers.ToListAsync();
+            var products = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Manufacturer)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                products = products.Where(p => p.Name.Contains(search) ||
+                p.Category.Name.Contains(search) ||
+                p.Manufacturer.Name.Contains(search));
+            }
+
+            if (categories?.Count > 0)
+            {
+                products = products.Where(p => categories.Contains(p.CategoryId));
+            }
+            if (manufacturers?.Count > 0)
+            {
+                products = products.Where(p => manufacturers.Contains(p.ManufacturerId));
+            }
+            return View(await PaginatedList<Product>.CreateAsync(products, page.Value, 12));
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Manufacturer)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+      
+    }
+}
